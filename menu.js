2f8c82bc635a54447abcb479f2a885a320474c06
Yulong Tan <yulongtan67@gmail.com>
2016-09-26 13:55:41 -0700

    fix undefined bug

--- menu.js
+++ menu.js
@@ -17,25 +17,23 @@
 	function displayItems() {
 		console.log('status: ' + this.status);
 		console.log(JSON.parse(this.reponseText));
-		if (this.status = 200) {
+		if (this.status == 200) {
 			var data = JSON.parse(this.responseText);
 			var table = document.querySelector('#menu');
-			for (var i = 1; i <= data.length; i++) {
-				if (data[i].name !== "") {
-					var tr = document.createElement('tr');
-					var num = document.createElement('td');	
-					var name = document.createElement('td');
-					var price = document.createElement('td');
+			for (var i = 0; i < data.length - 1; i++) {
+				var tr = document.createElement('tr');
+				var num = document.createElement('td');	
+				var name = document.createElement('td');
+				var price = document.createElement('td');
 					
-					num.innerHTML = (i + 1);
-					name.innerHTML = data[i].name;
-					price.innerHTML = data[i].price;
+				num.innerHTML = (i + 1);
+				name.innerHTML = data[i].name;
+				price.innerHTML = data[i].price;
 					
-					table.appendChild(tr);
-					tr.appendChild(num);
-					tr.appendChild(name);
-					tr.appendChild(price);
-				}
+				table.appendChild(tr);
+				tr.appendChild(num);
+				tr.appendChild(name);
+				tr.appendChild(price);
 			}
 		} else {
 			console.log('There was an error: ' + this.status);
