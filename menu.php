501dcc451bf46a1cc461dbb2e5afbc3cbe3a2e32
Yulong Tan <yulongtan67@gmail.com>
2016-09-26 13:22:23 -0700

    parsing through the txt file

---
+++ menu.php
@@ -0,0 +1,18 @@
+<?php
+
+# $start = $_GET["start"];
+# $end = $_GET["end"];
+$items = explode("\n", file_get_contents("menu.txt"));
+
+$data;
+foreach ($items as $item) {
+	$contents = explode("|", $item);
+	$data = array(
+		"name" => $contents[0],
+		"price" => $contents[1]
+	);
+}
+header("Content-type: Application/json");
+print json_encode($data);
+
+?>
